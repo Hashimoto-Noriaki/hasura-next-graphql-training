a8ff95466e42653525b1d9bb4a6599eef0d99c72
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-15 02:25:34 +0900

    ReactNative導入

---
+++ mobile/metro.config.js
@@ -0,0 +1,12 @@
+/**
+ * Metro configuration for React Native
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @format
+ */
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // 対応する拡張子を定義
+    },
+  };
+  
